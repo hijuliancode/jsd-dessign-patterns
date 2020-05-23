a0dfe564e5363dbd1be7cbf514986422929f5586
JulianCode <hijuliansosa@protonmail.com>
2020-05-23 15:53:47 -0500

    DP module 01

--- js/module.js
+++ js/module.js
@@ -8,14 +8,20 @@ const comprarBoleto = (function() {
   // Privado
   let evento = 'Conferencia JS 2021'
   let precio = 300
-
+  const adquirirBoleto = () => {
+    const elemento = document.createElement('p')
+    elemento.textContent = `Comprando boleto para ${evento}`
+    document.querySelector('#app').appendChild(elemento)
+  }
   // Publico
   return {
     mostrarBoleto: () => {
-      console.log(evento)
+      adquirirBoleto()
     }
   }
 })();
 
+// Todo lo que haya en el return lo podemos acceder, pero no sus variables privadas
+
 comprarBoleto.mostrarBoleto() // muestra informacion
-console.log(precio) // no puede acceder a variable privada
+console.log(comprarBoleto.evento) // undefined:
