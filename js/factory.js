14c7a5dd590b0c6220680e6e117a78580495109c
JulianCode <hijuliansosa@protonmail.com>
2020-05-23 16:25:30 -0500

    DP factory 01

--- js/factory.js
+++ js/factory.js
@@ -4,6 +4,9 @@
 // del programa, se decide que clase se va a instanciar
 // IMPORTANTE que sean objetos de tipo similar
 
+// Esa es la idea del Factory, no saber aún que clases se van a instanciar, pero
+// conforme el programa se ejecute, tener distintos tipos de datos que se pueden generar
+
 function ConstructorSitios() {
   this.crearElemento = function(texto, tipo) {
     let html;
@@ -21,10 +24,34 @@ function ConstructorSitios() {
         html = new ParrafoHTML(texto)
         break;
     }
+    html.tipo = tipo;
+
+    html.mostrar = function () {
+      const elemento = document.createElement(this.tipo)
+      switch (this.tipo) {
+        case 'input':
+          elemento.setAttribute('placeholder', this.texto)
+          break;
+        case 'img':
+          elemento.setAttribute('src', this.texto)
+          break;
+        default:
+          elemento.appendChild(document.createTextNode(this.texto))
+          break;
+      }
+      document.querySelector('#app').appendChild(elemento)
+    }
+
     return html;
   }
 }
 
+const InputHTML = function (texto) {
+  this.texto = texto
+}
+const ImagenHTML = function (texto) {
+  this.texto = texto
+}
 const HeadingHTML = function (texto) {
   this.texto = texto
 }
@@ -39,5 +66,11 @@ const elementosWeb = [];
 
 elementosWeb.push(sitioWeb.crearElemento('Hola Mundo', 'h1'))
 elementosWeb.push(sitioWeb.crearElemento('Aprendiendo Patrones de diseño con JavaScript', 'p'))
+elementosWeb.push(sitioWeb.crearElemento('meraki.svg', 'img'))
+elementosWeb.push(sitioWeb.crearElemento('Contactanos en el formulario', 'p'))
+elementosWeb.push(sitioWeb.crearElemento('Contacto', 'input'))
 
 console.log(elementosWeb);
+elementosWeb.forEach(elm => {
+  elm.mostrar()
+})
