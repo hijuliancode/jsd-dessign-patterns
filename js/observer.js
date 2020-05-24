330606c6617b7e1c872f31c1911b409f0a9de5d0
JulianCode <hijuliansosa@protonmail.com>
2020-05-24 13:21:17 -0500

    DP observer 01 - Patrón de Publicador - Suscriptor

--- js/observer.js
+++ js/observer.js
@@ -1,31 +1,31 @@
-// Observer también se le conoce como suscriptor-publicador
+// Observer: también se le conoce como suscriptor-publicador
 // Es un patro de comportamiento
 
 let observer = {
-  obtenerOfertas = function(callback) {
+  obtenerOfertas: function(callback) {
     if (typeof callback === 'function') {
       this.subscribers[this.subscribers.length] = callback;
     }
   },
-  cancelarOferta = function(callback) {
+  cancelarOfertas: function(callback) {
     for(let i = 0; i < this.subscribers.length; i++) {
       if (this.subscribers[i] === callback) {
-        delete this.subscribers[i]
+        delete this.subscribers[i];
       }
     }
   },
-  publicarOferta = function(oferta) {
+  publicarOferta: function(oferta) {
     for(let i = 0; i < this.subscribers.length; i++) {
       if (typeof this.subscribers[i] === 'function') {
         this.subscribers[i](oferta)
       }
     }
   },
-  crear: function(obj) {
+  crear: function(objeto) {
     for (let i in this) {
       if (this.hasOwnProperty(i)) {
-        obj[i] = i
-        obj.subscribers = []
+        objeto[i] = this[i]
+        objeto.subscribers = []
       }
     }
   }
@@ -33,14 +33,14 @@ let observer = {
 
 // Vendedores
 const udemy = {
-  nuevoCurso = function() {
+  nuevoCurso: function() {
     const curso = 'Nuevo curso de Javascript!!';
     this.publicarOferta(curso)
   }
 }
 
 const facebook = {
-  nuevoAnuncio = function() {
+  nuevoAnuncio: function() {
     const oferta = 'Compra un celular!!!';
     this.publicarOferta(oferta)
   }
@@ -49,3 +49,37 @@ const facebook = {
 // Crear los publicadores
 observer.crear(udemy)
 observer.crear(facebook)
+
+const meraki = {
+  compartir: function(oferta) {
+    console.log('Meraki dice, excelente oferta ' + oferta)
+  }
+}
+const ginna = {
+  interesa: function(oferta) {
+    console.log('Ginna dice, me interesa la oferta de ' + oferta)
+  }
+}
+const julian = {
+  interesa: function(oferta) {
+    console.log('Julian dice, me interesa la oferta de ' + oferta)
+  },
+  compartir: function(oferta) {
+    console.log('Julian dice, excelente oferta ' + oferta)
+  }
+}
+
+udemy.obtenerOfertas( meraki.compartir )
+udemy.obtenerOfertas( ginna.interesa )
+udemy.obtenerOfertas( julian.interesa )
+udemy.nuevoCurso()
+
+console.log('---');
+
+udemy.cancelarOfertas( julian.interesa )
+udemy.nuevoCurso()
+
+console.log('---');
+facebook.obtenerOfertas( ginna.interesa )
+facebook.obtenerOfertas( julian.compartir )
+facebook.nuevoAnuncio()
